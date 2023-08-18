b988fea2a26ec22768ae1344844b4f21edb0254a
Mario Cesar <mario.cesarnobrega@gmail.com>
2023-08-18 11:10:46 -0300

    Aula_Java

--- 1_programa.java
+++ 1_programa.java
@@ -1,11 +1,42 @@
 class main
 { 
     public static void main(String[] args)
-    {   //Forma 1
+    {   
+        //Array de Stings
+        
+        System.out.println(args);
+        for(int i=0; i<args.length; i++)
+        {
+            System.out.print(args[i]);
+            {
+                if(i < args.length-1)
+                {
+                    System.out.print(" e ");
+                }
+                else
+                {
+                    System.out.print(" -- ");
+                }
+            }
+        }
+        for(int i =0; i<args.length; i++)
+        {
+            System.out.print(args[i] + " e ");
+        }
+        System.out.println();
+        
+        
+        
+        //ARRAY DE INTEIROS
+        int notas[] = {7,10,9};
+        System.out.println(notas);
+        for(int i =0; i<3; i++)
+        {
+            System.out.println(notas);
+        }
+        
+        //Forma 1
         System.out.println("Helllo World");
-    
-
-    
         //forma 2
         String hello = "Hello world";
         System.out.println(hello.toUpperCase());
