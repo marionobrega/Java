384a1eea4796c51bfed1cc6ec994439babb4e95f
Mario Cesar <mario.cesarnobrega@gmail.com>
2023-08-18 10:44:48 -0300

    Fist Commit

---
+++ 1_programa.java
@@ -0,0 +1,13 @@
+class main
+{ 
+    public static void main(String[] args)
+    {   //Forma 1
+        System.out.println("Helllo World");
+    
+
+    
+        //forma 2
+        String hello = "Hello world";
+        System.out.println(hello.toUpperCase());
+    }
+}
